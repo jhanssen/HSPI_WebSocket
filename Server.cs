6fbc3847ae91e176c0dd7e8ea04ca567d9e1e18a
Jan Erik Hanssen <jhanssen@gmail.com>
2015-12-09 10:43:45 -0800

    Fix typo

--- Server.cs
+++ Server.cs
@@ -501,7 +501,7 @@ namespace HSPI_WebSocket2
                     List<object> grouped = new List<object>();
                     foreach (var gr in groups)
                     {
-                        grouped.Add(new { goupName = gr.GroupName, groupId = gr.GroupID, events = flat[gr.GroupID] });
+                        grouped.Add(new { groupName = gr.GroupName, groupId = gr.GroupID, events = flat[gr.GroupID] });
                     }
                     Send(JsonConvert.SerializeObject(new { id = id, type = "events", events = grouped }));
                 }
